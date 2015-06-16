cc9c7dcf82bb38a6289749c6c5ec2ce3a17d44c6
theCompanyDream <tbrantleyii@gmail.com>
2015-06-16 02:36:25 -0400

    Tic Tac assigning works properly now

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -22,8 +21,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+var node_modules = require('express-node_modules');
+
 app.use('/', routes);
 app.use('/users', users);
+app.use('/lib', node_modules);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,10 +41,8 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
+    console.error(err.message);
+
   });
 }
 
--- public/js/controller.js
+++ public/js/controller.js
@@ -1,3 +1,34 @@
-var angular = require("angular");
 
-var tictactoeapp = angular.module('tictactoeapp', []);
+var tictactoeapp = angular.module('tictactoeapp', [
+	]);
+var tiles = 9;
+
+tictactoeapp.controller('ttListCtrl', function($scope){
+	$scope.tiles = [];
+	$scope.defaultValue = ' ';
+	$scope.tictacchars = ['X','O'];
+	$scope.currentTurn = 0;
+	$scope.status = 'You Win';
+
+	$scope.assignValue = function (tile){
+
+		if(tile.value != $scope.defaultValue)
+			return;
+
+    	if($scope.currentTurn == $scope.tictacchars.length - 1)
+    		$scope.currentTurn = 0;
+    	else
+    		$scope.currentTurn+=1;
+
+    	tile.value = $scope.tictacchars[$scope.currentTurn];
+	}
+
+	for(var i=0; i < tiles; i++)
+	{
+		$scope.tiles.push({ 
+				'value' : $scope.defaultValue, 
+			    'currentPosition': i
+			});
+	}
+
+});
