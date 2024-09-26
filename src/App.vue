71b86accf599734da6841971dbc14195a5ff1f6e
theCompanyDream <tbrantleyii@gmail.com>
2024-09-26 14:18:09 -0400

    feat: more tailwind shit

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
-  <header>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </header>
-  <router-view />
+  <section class="flex flex-col min-h-screen ">
+    <header class="bg-blue-600 text-white p-4 bg-gray-100">
+      <h1 class="text-3xl font-bold text-center">GameLab</h1>
+    </header>
+    <router-view />
+    <footer class="bg-gray-800 text-white p-4 text-center">
+              &copy; 2024 GameLab. All rights reserved.
+    </footer>
+  </section>
 </template>
 
 <script>
@@ -20,6 +20,7 @@ export default {
 </script>
 
 <style scoped>
+/* @import './assets/main.css'; */
 .logo {
   height: 6em;
   padding: 1.5em;
