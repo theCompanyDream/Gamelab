cb81aa8f04c1196314090f280ac81a8541cb70f0
theCompanyDream <tbrantleyii@gmail.com>
2025-02-02 18:55:17 -0500

    feat: add vercel analytics

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import './main.css'
 import App from './App.vue'
 import router from './components/router'
+import { inject } from '@vercel/analytics'
+
+inject();
 
 createApp(App).use(router).mount('#app')
