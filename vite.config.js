6789186e16e6a528f37cb7fffd630dc8c2560fa4
theCompanyDream <tbrantleyii@gmail.com>
2025-04-14 21:52:14 -0400

    feat: better menu and about page

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url';
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
@@ -8,4 +9,9 @@ export default defineConfig({
     vue(),
     tailwindcss()
   ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
