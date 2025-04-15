0cd1a7ff679d3d3490f1c06bfb269ff882b2c936
theCompanyDream <tbrantleyii@gmail.com>
2025-04-15 09:20:31 -0400

    style: now pictures are visible

--- src/data/games.js
+++ src/data/games.js
@@ -3,18 +3,18 @@ export const games = [
 	  name: "Tic Tac Toe",
 	  slug: "tic-tac-toe",
 	  description: "Classic strategy game vs friend or bot.",
-	  image: "/thumbs/tictactoe.png"
+	  image: "/games/tictactoe.webp"
 	},
 	{
 	  name: "Rock Paper Scissors",
 	  slug: "rock-paper-scissors",
 	  description: "Test your luck and reflexes.",
-	  image: "/thumbs/rps.png"
+	  image: "/rps-sprite.webp"
 	},
 	{
 	  name: "Pong",
 	  slug: "pong",
 	  description: "Smash bricks with your paddle and ball.",
-	  image: "/thumbs/breakout.png"
+	  image: "/games/pong.webp"
 	}
   ];
